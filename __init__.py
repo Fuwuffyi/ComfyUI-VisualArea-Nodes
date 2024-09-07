1750de6dab427666a5e995f3a1a05a2240484969
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-09-07 20:05:29 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+@author: Fuwuffy 
+@title: Area Prompts
+@nickname: AreaPrompts 
+@version: 0.1
+@project: "https://github.com/crystian/ComfyUI-Crystools",
+@description: Nodes used for more comfortable area prompting design. 
+"""
