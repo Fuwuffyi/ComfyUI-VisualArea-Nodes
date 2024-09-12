810470032c0a88a934c330870968291b0532223d
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-09-12 14:47:21 +0200

    Improve python code quality

--- visual_area_prompt.py
+++ visual_area_prompt.py
@@ -16,7 +16,7 @@ class VisualAreaPrompt:
                 "unique_id": "UNIQUE_ID"
             },
         }
-    
+
     OUTPUT_TOOLTIPS = ("Area conditioning","Combined conditioning")
     RETURN_TYPES = ("CONDITIONING","CONDITIONING")
     RETURN_NAMES = ("area_conditioning","combined_conditioning")
@@ -38,8 +38,10 @@ class VisualAreaPrompt:
         graph: GraphBuilder = GraphBuilder()
         # Concat all other conditionings (to: (to: (to: general, from cond1), from cond2), from cond3... ... from global)
         last_concat: Node = graph.node("ConditioningConcat", conditioning_to=general_conditioning, conditioning_from=conditionings[0])
+        # Start loop from the second element (first already concatenated)
         for cond in conditionings[1:]:
             last_concat: Node = graph.node("ConditioningConcat", conditioning_to=last_concat.out(0), conditioning_from=cond)
+        # Combine with global conditioning
         combined_conditioning: Node = graph.node("ConditioningConcat", conditioning_to=last_concat.out(0), conditioning_from=global_conditioning)
         # Concat general to all other area conditionings (to: general, from: cond)
         conditionings_general: list = []
@@ -47,17 +49,27 @@ class VisualAreaPrompt:
             conditionings_general.append(graph.node("ConditioningConcat", conditioning_to=general_conditioning, conditioning_from=cond))
         # Apply area with percentage from conditionings
         conditionings_area: list = []
-        for i in range(0, len(conditionings_general)):
+        for i in range(len(conditionings_general)):
             cond = conditionings_general[i].out(0)
             area_values: list[float] = conditioning_areas[i]
-            conditionings_area.append(graph.node("ConditioningSetAreaPercentage", conditioning=cond, width=area_values[2], height=area_values[3], x=area_values[0], y=area_values[1], strength=area_values[4]))
+            conditionings_area.append(graph.node(
+                "ConditioningSetAreaPercentage",
+                conditioning=cond,
+                width=area_values[2],
+                height=area_values[3],
+                x=area_values[0],
+                y=area_values[1],
+                strength=area_values[4]
+            ))
         # Combine all conditionings together
-        last_combine: Node = graph.node("ConditioningCombine", conditioning_1=conditionings_area[0].out(0), conditioning_2=conditionings_area[1].out(0)) 
+        last_combine: Node = graph.node("ConditioningCombine", conditioning_1=conditionings_area[0].out(0), conditioning_2=conditionings_area[1].out(0))
+        # Start loop from the third element (two are already combined)
         for cond in conditionings_area[2:]:
             last_combine: Node = graph.node("ConditioningCombine", conditioning_1=last_combine.out(0), conditioning_2=cond.out(0))
+        # Final combination with the previously combined conditioning
         output: Node = graph.node("ConditioningCombine", conditioning_1=last_combine.out(0), conditioning_2=combined_conditioning.out(0))
+        # Return result and expanded graph
         return {
             "result": (output.out(0), combined_conditioning.out(0)),
             "expand": graph.finalize()
         }
-
